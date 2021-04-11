e430f33b959c49a29f1d00ddae62c946badc97df
Kamil Brzycki <kbrzycki@gmail.com>
2021-04-11 11:12:03 -0700

    Add jackson custom Calendar object deserializer to parse received json to Calendar object

---
+++ src/main/java/com/kami/brzycki/json/CalendarDeserializer.java
@@ -0,0 +1,53 @@
+package com.kami.brzycki.json;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.kami.brzycki.model.Calendar;
+import com.kami.brzycki.model.Meeting;
+import com.kami.brzycki.model.TimeSlot;
+import com.kami.brzycki.model.WorkingHours;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CalendarDeserializer extends StdDeserializer<Calendar> {
+
+    public CalendarDeserializer() {
+        this(null);
+    }
+
+    public CalendarDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+
+    @Override
+    public Calendar deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+
+        JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+        String workingHoursStart = node.get("working_hours").toString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        WorkingHours mappedWorkingHours = new WorkingHours(mapper.readValue(workingHoursStart, TimeSlot.class));
+
+        String plannedMeetings = node.get("planned_meeting").toString();
+        CollectionType javaType = mapper.getTypeFactory().constructCollectionType(List.class, TimeSlot.class);
+        List<TimeSlot> meetingTimeSlots = mapper.readValue(plannedMeetings, javaType);
+
+        List<Meeting> mappedPlannedMeetings = meetingTimeSlots
+                .stream()
+                .map(x -> {
+                    return new Meeting(x.getStart(), x.getEnd());
+                }).collect(Collectors.toList());
+
+        Calendar calendar = new Calendar(mappedWorkingHours, mappedPlannedMeetings);
+        return calendar;
+
+    }
+}
