7b66568517d0ff014dae8168ee9414832a80f171
Kamil Brzycki <kbrzycki@gmail.com>
2021-04-11 23:38:23 -0700

    Fixed problem deserialization problem that occured when json was not correct and could not be parsed to Calendar class

--- src/main/java/com/kami/brzycki/json/mapping/CalendarDeserializer.java
+++ src/main/java/com/kami/brzycki/json/mapping/CalendarDeserializer.java
@@ -7,10 +7,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.type.CollectionType;
+import com.kami.brzycki.error.JsonParseException;
 import com.kami.brzycki.model.Calendar;
 import com.kami.brzycki.model.Meeting;
 import com.kami.brzycki.model.TimeSlot;
 import com.kami.brzycki.model.WorkingHours;
+import lombok.SneakyThrows;
 
 import java.io.IOException;
 import java.util.List;
@@ -27,27 +29,32 @@ public class CalendarDeserializer extends StdDeserializer<Calendar> {
     }
 
 
+    @SneakyThrows
     @Override
     public Calendar deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
 
-        JsonNode node = jsonParser.getCodec().readTree(jsonParser);
-        String workingHoursStart = node.get("working_hours").toString();
+        try {
+            JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+            String workingHoursStart = node.get("working_hours").toString();
 
-        ObjectMapper mapper = new ObjectMapper();
-        WorkingHours mappedWorkingHours = new WorkingHours(mapper.readValue(workingHoursStart, TimeSlot.class));
+            ObjectMapper mapper = new ObjectMapper();
+            WorkingHours mappedWorkingHours = new WorkingHours(mapper.readValue(workingHoursStart, TimeSlot.class));
 
-        String plannedMeetings = node.get("planned_meeting").toString();
-        CollectionType javaType = mapper.getTypeFactory().constructCollectionType(List.class, TimeSlot.class);
-        List<TimeSlot> meetingTimeSlots = mapper.readValue(plannedMeetings, javaType);
+            String plannedMeetings = node.get("planned_meeting").toString();
+            CollectionType javaType = mapper.getTypeFactory().constructCollectionType(List.class, TimeSlot.class);
+            List<TimeSlot> meetingTimeSlots = mapper.readValue(plannedMeetings, javaType);
 
-        List<Meeting> mappedPlannedMeetings = meetingTimeSlots
-                .stream()
-                .map(x -> {
-                    return new Meeting(x.getStart(), x.getEnd());
-                }).collect(Collectors.toList());
+            List<Meeting> mappedPlannedMeetings = meetingTimeSlots
+                    .stream()
+                    .map(x -> {
+                        return new Meeting(x.getStart(), x.getEnd());
+                    }).collect(Collectors.toList());
 
-        Calendar calendar = new Calendar(mappedWorkingHours, mappedPlannedMeetings);
-        return calendar;
+            Calendar calendar = new Calendar(mappedWorkingHours, mappedPlannedMeetings);
+            return calendar;
+        } catch (Exception e) {
 
+            throw new JsonParseException("Could not parse json calendar");
+        }
     }
 }
