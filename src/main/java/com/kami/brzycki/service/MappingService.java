b29820b1c9f6e8cadbddaee61726fb5078b288e5
Kamil Brzycki <kbrzycki@gmail.com>
2021-04-11 23:46:21 -0700

    Class rename. Removed exception catching. Now throws custom JsonProcessingException

--- src/main/java/com/kami/brzycki/service/MappingService.java
+++ src/main/java/com/kami/brzycki/service/MappingService.java
@@ -1,4 +1,4 @@
-package com.kami.brzycki.Service;
+package com.kami.brzycki.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,14 +10,14 @@ import com.kami.brzycki.model.TimeSlot;
 
 import java.util.List;
 
-public class JsonMappingService {
+public class MappingService {
 
 
     private ObjectMapper mapper;
     private SimpleModule module;
 
 
-    public JsonMappingService() {
+    public MappingService() {
         mapper = new ObjectMapper();
         module = new SimpleModule();
         module.addDeserializer(Calendar.class, new CalendarDeserializer());
@@ -25,27 +25,16 @@ public class JsonMappingService {
         mapper.registerModule(module);
     }
 
-    public Calendar deserializeJsonCalendar(String json) {
+    public Calendar deserializeJsonCalendar(String json) throws JsonProcessingException {
         Calendar calendar = null;
-
-        try {
-            calendar = mapper.readValue(json, Calendar.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+        calendar = mapper.readValue(json, Calendar.class);
 
         return calendar;
     }
 
-    public String serializeAvailableMeetingsToJson(List<TimeSlot> availableMeetingTimeSlots) {
-
+    public String serializeAvailableMeetingTimeSlotsToJson(List<TimeSlot> availableMeetingTimeSlots) throws JsonProcessingException {
         String returnedJson = "";
-
-        try {
-            returnedJson = mapper.writeValueAsString(availableMeetingTimeSlots);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+        returnedJson = mapper.writeValueAsString(availableMeetingTimeSlots);
 
         return returnedJson;
     }
